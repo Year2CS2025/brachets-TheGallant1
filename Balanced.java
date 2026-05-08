9b4286c815bb0c514bc60261c02e38599c3f4a41
TheGallant1 <mfaabel1@gmail.com>
2026-05-08 10:17:47 +0200

    Update Balanced.java

--- Balanced.java
+++ Balanced.java
@@ -1,5 +1,5 @@
 import java.util.Stack;
-public class Balanced{
+public class Balanced {
     public static void main(String[] args) {
         String[] str = {"({[]})","({[})", "((()))", "({)}"};
         for(String s: str)
@@ -7,6 +7,24 @@ public class Balanced{
         
     }
     public static boolean isBalanced(String s){
+        Stack<Character> stack = new Stack<>();
+        for(char c: s.toCharArray()){
+            if(c == '(' || c == '{' || c == '['){
+                stack.push(c);
+            }else if(c == ')' && !stack.isEmpty() && stack.peek() == '('){
+                stack.pop();
+            }else if(c == '}' && !stack.isEmpty() && stack.peek() == '{'){
+                stack.pop();
+            }else if(c == ']' && !stack.isEmpty() && stack.peek() == '['){
+                stack.pop();
+            }else{
+                return false;
+            }
+        }
+        if(stack.isEmpty()){
+            return true;
+        }
+
         //
         
         return false;
